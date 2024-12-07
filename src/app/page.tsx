3f61dc0c0b63006a8d23f709cbe7e2ffec8483f3
Unicorn4rich <sk9952908@gmail.com>
2024-12-07 20:12:04 +0500

    new

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+
+       <h1>Shoaib</h1>
+        {/* <Hero/> */}
+
+    </>
+  );
+}
